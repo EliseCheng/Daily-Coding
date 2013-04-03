1709fe499484942da715f5675e9a80f885e415d1
Alice <chengwen.hs@gmail.com>
2013-04-03 13:42:28 +0800

    change its format

--- python/vat.py
+++ python/vat.py
@@ -1,10 +1,10 @@
 #! /usr/bin/python
 
-tax_d = {		"Belgium" : 0.21,
-						"Bulgaria" : 0.20,
-						"Czech Republic" : 0.20,
-						"Denmark"	: 0.25	
-				}
+tax_d = {	"Belgium" : 0.21,
+		"Bulgaria" : 0.20,
+		"Czech Republic" : 0.20,
+		"Denmark" : 0.25	
+	}
 
 def calculate(area, price):
 	return price * (1 + tax_d[area])
