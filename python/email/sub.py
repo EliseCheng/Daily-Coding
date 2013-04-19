7bf350a70f5741522f4cc55f6ada1d043881c274
root <root@alice-vm.(none)>
2013-04-19 17:44:10 +0800

    edit

--- python/email/sub.py
+++ python/email/sub.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException
 
 browser = webdriver.Firefox()
-browser.get("https://staging.acdid.acdsystems.com/ACDID/admin/login.php")
+browser.get("login.php")
 
 #login
 browser.find_element_by_id("email").send_keys()
@@ -16,7 +16,7 @@ browser.find_element_by_id("password").send_keys("")
 browser.find_element_by_xpath("//input[@type='submit']").click()
 
 #Open Test Tools
-browser.get("https://staging.acdid.acdsystems.com/ACDID/admin/dev_testtools.php?am=6")
+browser.get("testtools.php?am=6")
 browser.find_element_by_id("cur_sub_date").send_keys("2013-04-19")
 browser.find_element_by_name("sen_sub_emails").click()
 
