d2571ee2cf56204e187ae2a69a980c8896d9d225
Chengwen <chengwen@fnic.cn>
2013-08-07 15:45:35 +0800

    update

--- shell/install_libvirt.sh
+++ shell/install_libvirt.sh
@@ -4,8 +4,10 @@ if [ `whoami` != "root" ]; then
 	echo "需要root权限！"
 	echo "退出..."
 	exit
-else [ ! -f libvirt-1.0.1.tar.gz ]  || [ ! -f qemu.conf ] || [ ! -f libvirtd.conf ] || [ ! -f libvirt-bin ]
-	echo "请检查文件： libvirt-1.0.1.tar.gz, qemu.conf, libvirtd.conf, libvirt-bin 是否在当前目录下。"
+fi
+
+if [ ! -f libvirt-1.0.1.tar.gz ]  || [ ! -f qemu.conf ] || [ ! -f libvirtd.conf ] || [ ! -f libvirt-bin ];then
+	echo "请检查文件： libvirt-1.0.1.tar.gz是libvirt安装包，qemu.conf, libvirtd.conf, libvirt-bin是修改好的配置文件，用来直接替换原有文件，请检查它们是否在当前目录下。"
 	exit
 fi 
 
@@ -13,7 +15,7 @@ apt-get update
 
 apt-get install -y libvirt-bin
 
-apt-get install gcc make pkg-config python-dev libxml2-dev libgnutls-dev libdevmapper-dev gnutls-bin libgnutls-dev virtinst pm-utils libyajl-dev libnl-dev
+apt-get install gcc make pip pkg-config libxml2-dev libgnutls-dev virtinst libdevmapper-dev gnutls-bin libgnutls-dev pm-utils libyajl-dev libnl-dev python-lxmllibxslt1-dev
 
 tar -zvxf libvirt-1.0.1.tar.gz
 cd libvirt-1.0.1
@@ -33,19 +35,21 @@ echo
 cp /etc/default/libvirt-bin /etc/default/libvirt-bin.bak
 cp ../libvirt-bin /etc/default/libvirt-bin
 
+cd ../
 service libvirt-bin restart
 
 LIBVIRT_PRO=`netstat -anput | grep libvirtd`
-if [ ! -n $LIBVIRT_PRO ];then
+if [ ! -n "$LIBVIRT_PRO" ];then
 	echo "libvirt进程没有成功启动。"
 else 
 	echo $LIBVIRT_PRO
 	echo "libvirt进程成功启动。"	
-fi 
-
+fi
+echo "Waiting..." 
+sleep 3
 echo "=================设置远程访问libvirt=============="
 apt-get install sasl2-bin
-echo "设置访问libvirt的用户名和密码："
+echo "设置访问libvirt的密码，用户名是root："
 saslpasswd2 -a libvirt root
 
 echo "验证用户名密码设置是否成功："
@@ -53,7 +57,15 @@ sasldblistusers2 -f /etc/libvirt/passwd.db
 
 echo "验证连接libvirt命令"
 virsh -c qemu+tcp://127.0.0.1/system
-echo "会看到virsh的欢迎提示，用quit命令退出。"
+echo "看到virsh的欢迎提示了吗？有的话就成功了哦！:)"
+
+echo "===========开始安装glance=========="
+sleep 2
+wget https://github.com/openstack/glance/archive/2012.2.4.zip
+unzip 2012.2.4.zip
+cd glance-2012.2.4 
+python setup.py build
+sudo python setup.py install
 
 
 
